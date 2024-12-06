4015383abada8b0135ac19ef2272840643168f59
chenhaipei01 <15129544+chenhaipei01@user.noreply.gitee.com>
2024-12-06 22:14:01 +0800

    DELETE NO USE

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module v5sdk_go
 
-go 1.15
+go 1.22
 
 require (
 	github.com/gorilla/websocket v1.4.2
--- main.go
+++ main.go
@@ -5,39 +5,8 @@ import (
 	"fmt"
 	"log"
 	"time"
-	. "v5sdk_go/rest"
 	. "v5sdk_go/ws"
 )
-
-/*
-	rest API请求
-	更多示例请查看 rest/rest_test.go
-*/
-func REST() {
-	// 设置您的APIKey
-	apikey := APIKeyInfo{
-		ApiKey:     "xxxx",
-		SecKey:     "xxxx",
-		PassPhrase: "xxxx",
-	}
-
-	// 第三个参数代表是否为模拟环境，更多信息查看接口说明
-	cli := NewRESTClient("https://www.okex.win", &apikey, true)
-	rsp, err := cli.Get(context.Background(), "/api/v5/account/balance", nil)
-	if err != nil {
-		return
-	}
-
-	fmt.Println("Response:")
-	fmt.Println("\thttp code: ", rsp.Code)
-	fmt.Println("\t总耗时: ", rsp.TotalUsedTime)
-	fmt.Println("\t请求耗时: ", rsp.ReqUsedTime)
-	fmt.Println("\t返回消息: ", rsp.Body)
-	fmt.Println("\terrCode: ", rsp.V5Response.Code)
-	fmt.Println("\terrMsg: ", rsp.V5Response.Msg)
-	fmt.Println("\tdata: ", rsp.V5Response.Data)
-
-}
 
 // 订阅私有频道
 func wsPriv() {
@@ -62,7 +31,7 @@ func wsPriv() {
 		log.Println(err)
 		return
 	}
-	//defer r.Stop()
+	defer r.Stop()
 	var res bool
 
 	res, _, err = r.Login(apikey, secretKey, passphrase)
@@ -98,54 +67,6 @@ func wsPriv() {
 		fmt.Println("取消订阅失败！", err)
 	}
 
-}
-
-// 订阅公共频道
-func wsPub() {
-	
-	ep := "wss://wspap.okx.com:8443/ws/v5/public"
-
-	// 创建ws客户端
-	r, err := NewWsClient(ep)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	// 设置连接超时
-	r.SetDailTimeout(time.Second * 2)
-	err = r.Start()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer r.Stop()
-	// 订阅产品频道
-	var args []map[string]string
-	arg := make(map[string]string)
-	arg["instType"] = FUTURES
-	//arg["instType"] = OPTION
-	args = append(args, arg)
-
-	start := time.Now()
-	res, _, err := r.PubInstruemnts(OP_SUBSCRIBE, args)
-	if res {
-		usedTime := time.Since(start)
-		fmt.Println("订阅成功！", usedTime.String())
-	} else {
-		fmt.Println("订阅失败！", err)
-	}
-
-	time.Sleep(30 * time.Second)
-
-	start = time.Now()
-	res, _, err = r.PubInstruemnts(OP_UNSUBSCRIBE, args)
-	if res {
-		usedTime := time.Since(start)
-		fmt.Println("取消订阅成功！", usedTime.String())
-	} else {
-		fmt.Println("取消订阅失败！", err)
-	}
 }
 
 // websocket交易
@@ -188,11 +109,11 @@ func wsJrpc() {
 
 	start := time.Now()
 	param := map[string]interface{}{}
-	param["instId"] = "BTC-USDT"
-	param["tdMode"] = "cash"
+	param["instId"] = "BTC-USDT-SWAP"
+	param["tdMode"] = "cross"
 	param["side"] = "buy"
 	param["ordType"] = "market"
-	param["sz"] = "200"
+	param["sz"] = "0.1"
 	req_id = "00001"
 
 	res, _, err = r.PlaceOrder(req_id, param)
@@ -206,8 +127,6 @@ func wsJrpc() {
 }
 
 func main() {
-	// 公共订阅
-	wsPub()
 
 	// 私有订阅
 	wsPriv()
@@ -215,6 +134,5 @@ func main() {
 	// websocket交易
 	wsJrpc()
 
-	// rest请求
-	//REST()
+
 }
