9535b11ba9ad8ba7973c7a509d299d5eafe86221
chenhaipei01 <15129544+chenhaipei01@user.noreply.gitee.com>
2024-12-07 16:52:37 +0800

    main

--- main.go
+++ main.go
@@ -9,63 +9,6 @@ import (
 	. "v5sdk_go/ws"
 )
 
-// websocket交易
-func wsJrpc() {
-	ep := "wss://wspap.okx.com:8443/ws/v5/private"
-
-	// 填写您自己的APIKey信息
-	// 填写您自己的APIKey信息
-	apikey := "de6607bf-f39e-4781-83e3-b9b338319ae3"
-	secretKey := "2A13AF232E1AE4F4C105AA25B3645C01"
-	passphrase := "@Zxjchp1314520"
-
-	var res bool
-	var req_id string
-
-	// 创建ws客户端
-	r, err := NewWsClient(ep)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	// 设置连接超时
-	r.SetDailTimeout(time.Second * 2)
-	err = r.Start()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	defer r.Stop()
-
-	res, _, err = r.Login(apikey, secretKey, passphrase)
-	if res {
-		fmt.Println("websocket交易登录成功！")
-	} else {
-		fmt.Println("websocket交易登录失败！", err)
-		return
-	}
-
-	start := time.Now()
-	param := map[string]interface{}{}
-	param["instId"] = "BTC-USDT-SWAP"
-	param["tdMode"] = "cross"
-	param["side"] = "sell"
-	param["posSide"] = "short"
-	param["ordType"] = "market"
-	param["sz"] = "0.1"
-	req_id = "00001"
-
-	res, _, err = r.PlaceOrder(req_id, param)
-	if res {
-		usedTime := time.Since(start)
-		fmt.Println("下单成功！", usedTime.String())
-	} else {
-		usedTime := time.Since(start)
-		fmt.Println("下单失败！", usedTime.String(), err)
-	}
-}
 
 var (
     signalClients = make(map[string]*WsClient)
@@ -76,7 +19,7 @@ var (
 func monitorSignalAccounts() {
     for apiKey, client := range signalClients {
         go func(apiKey string, client *WsClient) {
-            for msg := range client.Messages() {
+            for msg := range client.Messages {
                 switch msg.Type {
                 case "orders":
                     handleOrder(apiKey, msg.Info)
@@ -200,7 +143,7 @@ func con_login_sub_f(config *jsonConfig) {
 		defer r.Stop()
 		var res bool
 		var err error
-		res, _, err = r.Login(account.APIKey, account.SecretKey, account.Passphrase)
+		res, _, err = r.Login(config.APIKey, config.SecretKey, config.Passphrase)
 		if res {
 			fmt.Println("跟单登录成功！")
 		} else {
